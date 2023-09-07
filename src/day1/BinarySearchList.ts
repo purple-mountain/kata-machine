1e5987367a3bb41d2b8c38c12f852b972c25e59b
purple-mountain <lmwrong92@gmail.com>
2023-09-07 20:35:35 +0500

    Binary search done

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,12 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let low = 0;
+    let high = haystack.length;
+    while (high > low) {
+        let middle = Math.floor((low + high) / 2);
+        let element = haystack[middle];
+        if (element === needle) return true;
+        else if (element > needle) high = middle;
+        else low = middle + 1;
+    }
+    return false;
 }
