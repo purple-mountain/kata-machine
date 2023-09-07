efefdb8f6a79fdd692d7fd02871b83a7660f1291
purple-mountain <lmwrong92@gmail.com>
2023-09-07 21:57:18 +0500

    Two crystal ballz done

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let jmpDist = Math.floor(Math.sqrt(breaks.length));
+    let i = jmpDist;
+    for (; i < breaks.length; i += jmpDist) {
+        if (breaks[i]) {
+            break;
+        }
+    }
 
+    i -= jmpDist;
+
+    for (let j = 0; j < jmpDist; j++, i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
