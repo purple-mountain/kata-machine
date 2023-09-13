f3ca99e8078daec9f4064f185191ca4a7acee3e2
purple-mountain <lmwrong92@gmail.com>
2023-09-13 19:20:10 +0500

    BubbleSort done

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,14 @@
 export default function bubble_sort(arr: number[]): void {
-
+    let temp;
+    let n = arr.length;
+    while (n > 1) {
+        for (let i = 0; i < n; i++) {
+            if (arr[i] > arr[i + 1]) {
+                temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        n--;
+    }
 }
