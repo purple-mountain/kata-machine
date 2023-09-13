172316edbfc2df50a18d5a33e099439fcb24d9b3
purple-mountain <lmwrong92@gmail.com>
2023-09-13 19:31:07 +0500

    Fix: BubbleSort. Unneccessary iteration

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -2,7 +2,7 @@ export default function bubble_sort(arr: number[]): void {
     let temp;
     let n = arr.length;
     while (n > 1) {
-        for (let i = 0; i < n; i++) {
+        for (let i = 0; i < n - 1; i++) {
             if (arr[i] > arr[i + 1]) {
                 temp = arr[i + 1];
                 arr[i + 1] = arr[i];
