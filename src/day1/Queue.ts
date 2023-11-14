48c20bb52a508853fb46ef07edd47787e6da6464
purple-mountain <lmwrong92@gmail.com>
2023-11-14 17:05:03 +0500

    Minor updates of queue implementation

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,43 +1,38 @@
 export default class Queue<T> {
     public length: number = 0;
-    private head: Node<T> | null = null;
-    private tail: Node<T> | null = null;
+    private head?: Node<T> | undefined = undefined;
+    private tail?: Node<T> | undefined = undefined;
 
     constructor() {}
 
     enqueue(item: T): void {
         const newNode = new Node<T>(item);
+        this.length++;
         if (!this.tail) {
-            this.head = newNode;
-            this.tail = newNode;
-            this.length++;
-        } else {
-            this.tail!.next = newNode;
-            this.tail = newNode;
-            this.length++;
+            this.head = this.tail = newNode;
+            return;
         }
+        this.tail.next = newNode;
+        this.tail = newNode;
     }
     deque(): T | undefined {
         if (!this.head) {
-            this.tail = null;
+            this.tail = undefined;
             return;
         }
-        const h = this.head;
-        this.head = this.head.next;
-        h.next = null;
         this.length--;
+        const h = this.head;
+        this.head = h.next;
+        h.next = undefined;
         return h.data;
     }
     peek(): T | undefined {
-        if (!this.head) {
-            return;
-        }
-        return this.head.data;
+        return this.head?.data;
     }
 }
 
 class Node<T> {
-    public next: Node<T> | null = null;
+    public next?: Node<T>;
     constructor(public data: T) {
         this.data = data;
     }
