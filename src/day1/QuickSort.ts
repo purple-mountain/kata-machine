601d0a55c0f691e4b3de52731c302c5497df21ae
purple-mountain <lmwrong92@gmail.com>
2024-03-28 01:31:53 +0500

    Quick Sort Done

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,35 @@
 export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length);
+}
 
+function partition(arr: number[], low: number, high: number): number {
+    const pivot = arr[high - 1];
+
+    let idx = low - 1;
+
+    for (let i = low; i < high - 1; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const temp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = temp;
+        }
+    }
+
+    idx++;
+    arr[high - 1] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+}
+
+function qs(arr: number[], low: number, high: number): void {
+    if (low >= high) {
+        return;
+    }
+
+    const pIdx = partition(arr, low, high);
+
+    qs(arr, low, pIdx);
+    qs(arr, pIdx + 1, high);
 }
